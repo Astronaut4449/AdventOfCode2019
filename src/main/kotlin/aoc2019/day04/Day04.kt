64a624efc542ecc9e174a9bc283029449bf257b9
Richard Schielek <richard.schielek@gmx.de>
2019-12-04 19:31:17 +0100

    Adds Day04.

---
+++ src/main/kotlin/aoc2019/day04/Day04.kt
@@ -0,0 +1,22 @@
+package aoc2019.day04
+
+import java.io.File
+
+private fun String.sorted() = toCharArray().sorted().joinToString("")
+
+fun isValidPassword1(password: String): Boolean =
+        (password == password.sorted()) && (password.length != password.toSet().size)
+
+fun isValidPassword2(password: String): Boolean =
+        isValidPassword1(password) && password.groupingBy { it }.eachCount().values.contains(2)
+
+
+fun main() {
+    val (start, end) = File("input/day04.txt").readText().split("-").map { it.toInt() }
+
+    val validPasswordCount1 = (start..end).count { isValidPassword1(it.toString()) }
+    val validPasswordCount2 = (start..end).count { isValidPassword2(it.toString()) }
+
+    println("Part 1: $validPasswordCount1")
+    println("Part 2: $validPasswordCount2")
+}
