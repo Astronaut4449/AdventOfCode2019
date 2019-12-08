cb3be28533c6601b2f827fa0a066f9112758b834
Richard Schielek <richard.schielek@gmx.de>
2019-12-08 15:23:03 +0100

    Adds Day08.

---
+++ src/main/kotlin/aoc2019/day08/Day08.kt
@@ -0,0 +1,47 @@
+package aoc2019.day08
+
+import java.io.File
+
+private const val WIDTH = 25
+private const val HEIGHT = 6
+private const val TRANSPARENT = 2
+private const val BLACK = 0
+
+val layers = File("input/day08.txt").readText()
+        .chunked(WIDTH * HEIGHT)
+        .map { it.chunked(WIDTH).map { it.toCharArray().map(Char::toString).map(String::toInt) } }
+
+fun main() {
+    val layerFewestZeros = layers
+            .map { layer -> layer.flatten() }
+            .minBy { pixel -> pixel.count { it == 0 } }!!
+
+    val charCount = layerFewestZeros.groupingBy { it }.eachCount()
+
+    println("Part 1: ${(charCount[1] ?: 0) * (charCount[2] ?: 0)}")
+
+    val image = Array(HEIGHT) { IntArray(WIDTH) }
+    for (row in 0 until HEIGHT) {
+        for (col in 0 until WIDTH) {
+            layers@ for (layer in layers) {
+                val pixel = layer[row][col]
+                if (pixel != TRANSPARENT) {
+                    image[row][col] = pixel
+                    break@layers
+                }
+            }
+        }
+    }
+
+    val imageBeautified = image.joinToString("\n") { row ->
+        row.joinToString(" ") { pixel ->
+            when (pixel) {
+                BLACK -> "."
+                else -> "#"
+            }
+        }
+    }
+
+    println("Part 2:\n$imageBeautified")
+}
+
