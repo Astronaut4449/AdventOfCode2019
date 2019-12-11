51e29e2155c3e090c64d61ac6d0e552e0287d6bb
Richard Schielek <richard.schielek@gmx.de>
2019-12-11 19:32:28 +0100

    Adds Day11.

---
+++ src/main/kotlin/aoc2019/day11/Day11.kt
@@ -0,0 +1,165 @@
+package aoc2019.day11
+
+import aoc2019.day11.Direction.*
+import java.io.File
+import java.util.*
+
+private class IntProcessor(intCode: List<Long>) {
+    lateinit var input: Sequence<Long>
+
+    private val intCodeMap = intCode.mapIndexed { index, value -> index to value }.toMap().toMutableMap()
+    private val iterator by lazy { input.iterator() }
+
+    fun run() = sequence {
+        var pointer = 0
+        var relativeBase = 0
+
+        process@ while (true) {
+            val opCode = intCodeMap.getOrDefault(pointer, 0).toString().padStart(5, '0')
+            val instruction = opCode.substring(3).toInt()
+
+            // Helper for position, immediate and relative access
+            val ref = object {
+                private val params = (1..3).map { intCodeMap.getOrDefault(pointer + it, 0) }
+                private val modes = opCode.chunked(1).take(3).map(String::toInt).reversed()
+
+                operator fun get(index: Int): Long = when (modes[index]) {
+                    0 -> intCodeMap.getOrDefault(params[index].toInt(), 0) // position
+                    1 -> params[index] // immediate
+                    2 -> intCodeMap.getOrDefault(params[index].toInt() + relativeBase, 0) // relative
+                    else -> error("Unknown read mode ${modes[index]}")
+                }
+
+                operator fun set(index: Int, value: Long) {
+                    when (modes[index]) {
+                        0 -> intCodeMap[params[index].toInt()] = value // position
+                        2 -> intCodeMap[params[index].toInt() + relativeBase] = value // relative
+                        else -> error("Unknown write mode ${modes[index]}")
+                    }
+                }
+            }
+
+            // Execute instruction
+            when (instruction) {
+                1 -> ref[2] = ref[0] + ref[1]
+                2 -> ref[2] = ref[0] * ref[1]
+                3 -> ref[0] = iterator.next()
+                4 -> yield(ref[0])
+                7 -> ref[2] = if (ref[0] < ref[1]) 1L else 0L
+                8 -> ref[2] = if (ref[0] == ref[1]) 1L else 0L
+                9 -> relativeBase += ref[0].toInt()
+                99 -> break@process
+            }
+
+            // Move pointer
+            when (instruction) {
+                1, 2, 7, 8 -> pointer += 4
+                3, 4, 9 -> pointer += 2
+                5 -> pointer = (if (ref[0] != 0L) ref[1].toInt() else pointer + 3)
+                6 -> pointer = (if (ref[0] == 0L) ref[1].toInt() else pointer + 3)
+            }
+        }
+    }
+}
+
+private data class Point(val x: Int, val y: Int) {
+    operator fun plus(direction: Direction) = when (direction) {
+        LEFT -> Point(x - 1, y)
+        RIGHT -> Point(x + 1, y)
+        UP -> Point(x, y + 1)
+        DOWN -> Point(x, y - 1)
+    }
+}
+
+private enum class Direction {
+    LEFT, RIGHT, UP, DOWN;
+
+    fun leftTurn() = when (this) {
+        LEFT -> DOWN
+        DOWN -> RIGHT
+        RIGHT -> UP
+        UP -> LEFT
+    }
+    fun rightTurn() = when (this) {
+        LEFT -> UP
+        UP -> RIGHT
+        RIGHT -> DOWN
+        DOWN -> LEFT
+    }
+
+}
+
+private enum class Turn { LEFT, RIGHT }
+
+private enum class Color { BLACK, WHITE }
+
+private class PaintRobot(val whitePanels: MutableSet<Point>) {
+    val panelsEverPaintedWhite = mutableSetOf<Point>()
+
+    fun paint() {
+        val intProcessor = IntProcessor(intCode)
+
+        val outputQueue = LinkedList<Long>()
+        intProcessor.input = sequence {
+            yield(if (Point(0, 0) in whitePanels) 1L else 0L)
+            while (outputQueue.isNotEmpty()) {
+                yield(outputQueue.removeFirst())
+            }
+        }
+
+        var currDirection = UP
+        var currPosition = Point(0, 0)
+        val output = intProcessor.run().iterator()
+        while (output.hasNext()) {
+            // read input
+            val paint = Color.values()[output.next().toInt()]
+            val turn = Turn.values()[output.next().toInt()]
+
+            // paint
+            when (paint) {
+                Color.WHITE -> {
+                    whitePanels.add(currPosition)
+                    panelsEverPaintedWhite.add(currPosition)
+                }
+                Color.BLACK -> whitePanels.remove(currPosition)
+            }
+
+            // move
+            currDirection = if (turn == Turn.LEFT) currDirection.leftTurn() else currDirection.rightTurn()
+            currPosition += currDirection
+
+            // identify next panel
+            outputQueue.add(if (currPosition in whitePanels) 1L else 0L)
+        }
+    }
+}
+
+val intCode = File("input/day11.txt").readText().split(',').map(String::toLong)
+
+fun main() {
+    run {
+        val robot = PaintRobot(whitePanels = mutableSetOf())
+        robot.paint()
+        println("Part 1: ${robot.panelsEverPaintedWhite.size}")
+    }
+
+    run {
+        val robot = PaintRobot(whitePanels = mutableSetOf(Point(0,0)))
+        robot.paint()
+
+        with (robot.whitePanels) {
+            val xMin = map { it.x }.min()!!
+            val xMax = map { it.x }.max()!!
+            val yMin = map { it.y }.min()!!
+            val yMax = map { it.y }.max()!!
+
+            println("Part 2:")
+            for (y in yMax downTo yMin) {
+                for (x in xMin..xMax) {
+                    print(if (Point(x, y) in this) "# " else "  ")
+                }
+                print("\n")
+            }
+        }
+    }
+}
